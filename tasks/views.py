e8a51864d819ac55d989f46e2ac7f07e14402456
Chris Mugavero <chrismugavero@Chris-Mugaveros-MacBook-Pro.local>
2020-10-07 10:18:11 -0500

    updates

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+tasks = ["foo", "barr", "baz"]
+
+
 # Create your views here.
+def index(request):
+    return render(request, "tasks/index.html",{
+        "tasks": tasks
+    })
